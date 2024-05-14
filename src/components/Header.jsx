00659c972c9f0e6cbdfc677b2ee0114896bbab22
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-15 00:43:48 +0200

    fix: header marges

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 const Header = () => {
   return (
     <header className="header">
-      <Logo />
+      <Logo size='large' color='#FF6060'/>
       <Navigation />
     </header>
   );
