58891710ccb59beaa9629629d156dc0e6bd6faed
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-15 01:33:49 +0200

    feat: create banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from '../components/Banner';
 
 const Home = () => {
   return (
-    <div>
-      Home Sweet Home
-    </div>
+    <main>
+      <Banner />
+    </main>
   );
 };
 
