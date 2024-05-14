b7df16f7593d25ab97c67b750a51bf3974fcaef3
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-15 00:43:32 +0200

    feat: create footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Header from './components/Header';
+import Footer from './components/Footer';
 const App = () => {
 	return (
 		<BrowserRouter>
@@ -12,6 +13,7 @@ const App = () => {
 				<Route path='/about' element={<About />} />
 				<Route path='*' element={<NotFound />} />
 			</Routes>
+			<Footer />
 		</BrowserRouter>
 	);
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from './Logo';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Logo size="small" color="white" />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
