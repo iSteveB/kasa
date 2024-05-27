46c2112fe884090f2c7edc64a927f9ce502e2c7c
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-27 14:41:00 +0200

    style: responsive home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Banner from '../components/Banner';
+import Collapse from '../components/Collapse';
+import AboutBanner from '../assets/images/about_banner.jpg'
 
 const About = () => {
   return (
     <div>
-      What About Us
+      <Banner image={AboutBanner} />
+      <div className="faq">
+        <Collapse title="Fiabilité" text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
+        <Collapse title="Respect" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+        <Collapse title="Service" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+        <Collapse title="Sécurité" text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standars sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+      </div>
     </div>
   );
 };
