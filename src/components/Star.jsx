3d5ac60355c6493e9153e55ae34ad35a98757f38
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-05-27 18:36:43 +0200

    style: responsive details page

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -4,8 +4,7 @@ function Star({ filled }) {
 	return (
 		<svg
 			xmlns='http://www.w3.org/2000/svg'
-			width='36'
-			height='36'
+			className='rating-star'
 			fill='none'
 			viewBox='0 0 36 36'>
 			<path
